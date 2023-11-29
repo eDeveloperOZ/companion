97ffda7d43c3362bd35eb30930831ff7bd45a863
ofir ozeri <ofiroz91@gmail.com>
2023-11-29 18:58:38 +0200

    fixed bug for echoing multi messages

--- scripts/temp_translator.py
+++ scripts/temp_translator.py
@@ -1,7 +1,6 @@
 from telethon import TelegramClient, events
 from deep_translator import GoogleTranslator
 import os 
-improt asyncio
 
 DEV = False
 if DEV:
@@ -18,32 +17,23 @@ else:
         pass
 
 monitored_channel_id = ['gazaalannet','companion_dev', 'gazaalannetgroup', 'gazaalanpa']
+
 tarfet_channel_id = 'globchaniniw'
-echoed_channels = {}
 
 client = TelegramClient('anon', APP_ID, API_HASH)
 
 @client.on(events.NewMessage(chats=monitored_channel_id))
 async def new_message_listener(event):
     # print(event)
-    try:
-        channel_id = event.chat_id
-        # Check if we already echoed for this channel
-        if channel_id not in echoed_channels:
-            await asyncio.sleep(1)
-            await client.send_message(tarfet_channel_id, f"From: {event.chat.title}")
-            # Mark this channel as echoed
-            echoed_channels[channel_id] = True
+    try: 
         translated_text = GoogleTranslator(source='auto', target='iw').translate(event.message.message)
         event.message.message = translated_text
-        
-        # Echo the translated message
+        # print channel name 
+        await client.send_message(tarfet_channel_id, f"From: {event.chat.title}")
         await client.send_message(tarfet_channel_id, event.message)
     except Exception as e:
         print(e)
         pass
-    finally:
-        echoed_channels.pop(channel_id, None)
     
 
 client.start()
