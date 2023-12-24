74c06b68608552f676c18893ad3fc10615cfc22f
ofir ozeri <ofiroz91@gmail.com>
2023-12-24 22:38:25 +0200

    feature: new refactored project, not tested yet

--- src/app.py
+++ src/app.py
@@ -1,12 +1,43 @@
-# src/app.py
+# app.py
+from telethon import TelegramClient, events
 from translator import ChannelTranslator
 from duplicate_scanner import DuplicateScanner
+from .utils.config import Config
+from .utils.constants import TARGET_CHANNEL_ID
+import asyncio
 
 class App:
     def __init__(self):
+        self.client = TelegramClient('anon', Config.APP_ID, Config.API_HASH)
         self.translator = ChannelTranslator()
         self.scanner = DuplicateScanner()
+        self.monitored_channel_id = Config.get_monitored_channels()
+        self.processed_group_ids = set()
+        self.client.add_event_handler(self.new_message_listener, events.NewMessage(chats=self.monitored_channel_id))
 
-    def run(self):
-        self.translator.start()
-        self.scanner.start()
+
+    async def new_message_listener(self, event):
+        print(f"Received message ID: {event.message.id} with Group ID: {event.message.grouped_id}")
+
+        group_id = event.message.grouped_id
+        if group_id is None:
+            group_id = event.message.id
+            is_new_group_or_message = True
+        else:
+            is_new_group_or_message = group_id not in self.processed_group_ids
+
+        if is_new_group_or_message:
+            self.processed_group_ids.add(group_id)
+            last_messages = await self.client.get_messages(TARGET_CHANNEL_ID, limit=25)
+            if not await self.translator.is_message_similar(event.message.message, last_messages):
+                translated_text = await self.translator.translate_message(event.message.message)
+                if translated_text:
+                    event.message.message = translated_text
+                    await self.client.send_message(TARGET_CHANNEL_ID, event.message)
+
+    async def run(self):
+        await self.client.start()
+        scanner_task = asyncio.create_task(self.scanner.run())
+        await self.client.run_until_disconnected()
+        await scanner_task
+
--- src/utils/config.py
+++ src/utils/config.py
@@ -1,25 +1,29 @@
 from dotenv import load_dotenv
-import os 
+import os
 
-class Config():
+class Config:
     DEV = False
-
-    def load_enviroment(self):
-        if DEV:
-            load_dotenv()
-        Config.ACCESS_TOKEN = os.getenv('API_TOKEN', 'default_token')
-        Config.APP_ID = os.getenv('APP_ID', 'default_app_id')
-        Config.API_HASH = os.getenv('APP_HASH', 'default_api_hash')
-
-    def get_monitored_channels(self):
-        return MONITORED_CHANNEL_IDS = [
-    'gazaalannet', 'companion_dev', 'Hezbollah', 'gazaalanpa',
-    'Electrohizbullah','farsna', 'parsine', 'Tasnimnews',
-    'gazatv2', 'AjaNews', 'almanarnews','GazaNewsNow', 
-    'ahfadalbahaa', 'sepah_pasdaran', 'NewsPs0', 'MohamadDabaa',
-    'hamaas_ps', 'kataebabuali2', 'abuhamzasarayaa', 'alidaralipress',
-    'TahrirPulse1', 'Eye_of_resistance','From_hebron', 'telepressnews',
-    'FarsNewsInt', 
+    MONITORED_CHANNEL_IDS = [
+        'gazaalannet', 'companion_dev', 'Hezbollah', 'gazaalanpa',
+        'Electrohizbullah', 'farsna', 'parsine', 'Tasnimnews',
+        'gazatv2', 'AjaNews', 'almanarnews', 'GazaNewsNow',
+        'ahfadalbahaa', 'sepah_pasdaran', 'NewsPs0', 'MohamadDabaa',
+        'hamaas_ps', 'kataebabuali2', 'abuhamzasarayaa', 'alidaralipress',
+        'TahrirPulse1', 'Eye_of_resistance', 'From_hebron', 'telepressnews',
+        'FarsNewsInt',
     ]
-# Load the enviroment variables when the module is imported
-Config().load_enviroment()
+
+    @classmethod
+    def load_environment(cls):
+        if cls.DEV:
+            load_dotenv()
+        cls.APP_ID = os.getenv('APP_ID', 'default_app_id')
+        cls.API_HASH = os.getenv('API_HASH', 'default_api_hash')
+        cls.ACCESS_TOKEN = os.getenv('API_TOKEN', 'default_token')
+
+    @classmethod
+    def get_monitored_channels(cls):
+        return cls.MONITORED_CHANNEL_IDS
+
+# Load the environment variables when the module is imported
+Config.load_environment()
