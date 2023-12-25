574f970423d8257049737c7af2e603c44670b199
ofir ozeri <ofiroz91@gmail.com>
2023-12-25 17:12:19 +0200

    refactor: removed old files from prev project structure

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 # This script checks if the current git branch is 'devel'. If it is, it ensures that the git status is clean,
-# then pushes changes to the new 'backup' branch (holding the previous 'main' content). If successful, it then pushes changes to the 'main' branch.
+# then pushes changes from 'devel' to 'main' and from 'main' to 'backup'.
 
 # Get the current working directory and move up one directory level
 WORK_DIR=$(pwd)
 cd "$WORK_DIR/.."
 
-# get the name of the current branch
-BRANCH_NAME=$(git branch --show-current)
-
-# Function to push changes from devel branch to given branch
+# Function to push changes from one branch to another
 push_changes() {
-    local target_branch=$1
-    if git push origin devel:"$target_branch"; then
-        echo "Pushed changes to $target_branch"
+    local source_branch=$1
+    local target_branch=$2
+    if git push origin "$source_branch":"$target_branch"; then
+        echo "Pushed changes from $source_branch to $target_branch"
     else
-        echo "Failed to push changes to $target_branch"
+        echo "Failed to push changes from $source_branch to $target_branch"
         exit 1
     fi
 }
@@ -27,19 +25,10 @@ if [ "$BRANCH_NAME" = "devel" ]; then
         echo "Git status is not clean, please commit changes before deploying"
         exit 1
     else
-        # Rename the current 'backup' branch to 'old-main' (or any other name you prefer)
-        git branch -m backup old-main
-        
-        # Create a new 'backup' branch from the current 'main' branch
-        git checkout main
-        git branch backup
-        
-        # Push changes from 'devel' to the new 'backup' branch
-        git checkout devel
-        push_changes backup
-        
-        echo "Pushing changes to main branch"
-        push_changes main
+        echo "Pushing changes from main to backup"
+        push_changes main backup
+        echo "Pushing changes from devel to main"
+        push_changes devel main
     fi
 else
     echo "Not on branch devel, please checkout devel before deploying"
